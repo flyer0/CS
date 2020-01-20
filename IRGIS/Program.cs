0bf50071a3acd634bc769b6bf0e187997ed311d2
flyer0 <flyer0@no-reply.github.com>
2020-01-20 17:22:12 +0100

    first cs app

---
+++ IRGIS/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IRGIS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
