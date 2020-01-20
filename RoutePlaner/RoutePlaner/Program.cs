e35455b5231f140af94774270201493fd0788eb8
flyer0 <flyer0@no-reply.github.com>
2020-01-20 19:21:39 +0100

    calculation two points

---
+++ RoutePlaner/RoutePlaner/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace RoutePlaner
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            WayPoint pratteln = new WayPoint("Pratteln", 47.5167, 7.6833);
+
+            WayPoint bern = new WayPoint("Bern", 46.95, 7.44);
+
+            WayPoint berlin = new WayPoint("Berlin", 52.52, 13.38);
+
+            Console.WriteLine($"Distance {bern.Name} -- {berlin.Name}: " + $"{bern.Distance(berlin):####.#} km");
+
+            Console.WriteLine(pratteln);
+            Console.ReadKey();
+        }
+    }
+}
